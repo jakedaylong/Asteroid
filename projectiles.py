f167618f1204451bfa9ce08de0607972240b57d8
Jake Daylong <jakedaylong@gmail.com>
2024-11-07 22:59:31 -0800

    added hit sound effect to laser and missile

--- projectiles.py
+++ projectiles.py
@@ -15,6 +15,7 @@ class Laser(pygame.sprite.Sprite):
         laser_sound.play()
         self.laser_hit = pygame.image.load('assets/projectiles/laser_hit.png').convert_alpha()
         self.laser_hit_rect = self.laser_hit.get_rect()
+        self.laser_hit_sound = pygame.mixer.Sound('assets/projectiles/explosion.wav')
         self.speed = 10
         self.rect = self.image.get_rect()
         self.rect.center = (x, y - (self.image.get_height() / 2))
@@ -27,6 +28,7 @@ class Laser(pygame.sprite.Sprite):
         for target in enemy_group:
             if target.rect.top < self.rect.centery < target.rect.bottom and target.rect.left < self.rect.centerx < target.rect.right:
                 screen_prop.screen.blit(self.laser_hit, (self.rect.centerx - self.laser_hit_rect.centerx, self.rect.centery - self.laser_hit_rect.centery))
+                self.laser_hit_sound.play()
                 self.kill()
 
 class Missile(pygame.sprite.Sprite):
@@ -39,6 +41,7 @@ class Missile(pygame.sprite.Sprite):
         laser_sound.play()
         self.laser_hit = pygame.image.load('assets/projectiles/laser_hit.png').convert_alpha()
         self.laser_hit_rect = self.laser_hit.get_rect()
+        self.laser_hit_sound = pygame.mixer.Sound('assets/projectiles/explosion.wav')
         self.speed = 0
         self.rect = self.image.get_rect()
         self.rect.center = (x, y - (self.image.get_height() / 2))
@@ -54,4 +57,5 @@ class Missile(pygame.sprite.Sprite):
             if target.rect.top < self.rect.centery < target.rect.bottom and target.rect.left < self.rect.centerx < target.rect.right:
                 screen_prop.screen.blit(self.laser_hit, (
                 self.rect.centerx - self.laser_hit_rect.centerx, self.rect.centery - self.laser_hit_rect.centery))
+                self.laser_hit_sound.play()
                 self.kill()
